5a29c99a04d8b3304ee53f8ab708a0f85ad41fe3
Arpedia <plane629@live.jp>
2019-09-05 21:56:57 +0900

    2 method test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,7 +12,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | Rails Tutrial"
   end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Rails Tutrial"
+  end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Rails Tutrial"
+  end
 
 
 end
