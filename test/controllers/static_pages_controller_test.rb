a3ea9e5c6cf78d96d52b16b943cad6565d678151
Arpedia <plane629@live.jp>
2019-09-05 17:19:07 +0900

    title set

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Home | Rails Tutrial"
+    assert_select "title", "Help | Rails Tutrial"
   end
 
 
