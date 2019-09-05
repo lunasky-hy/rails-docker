ee7a12368040c3d81bab4d5f962cd6f77728ac72
Arpedia <plane629@live.jp>
2019-09-05 17:26:09 +0900

    title helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def full_title(page_title = '')
+      base = "Rails Tutrial"
+      if page_title.empty? then
+        base
+      else
+        page_title + " | " + base
+      end
+    end
 end
